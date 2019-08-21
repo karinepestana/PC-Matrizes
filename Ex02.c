9e9d2c8f3d0ceb9068d8b5f6a4f6e7a8001c6f26
karinepestana <karine_pestana_34@hotmail.com>
2019-08-21 16:57:00 -0300

    +1

---
+++ Ex02.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main()
+{
+	int l, c, i, j, cont = 0;
+
+	printf("L: \n");
+	scanf("%d", &l);
+	printf("C: \n");
+	scanf("%d", &c);
+
+	float matriz[l][c];
+	float media[l];
+
+	for (i = 0; i < l; i++)
+		media[i] = 0;	
+
+	for (i = 0; i < l; i++) {
+		for (j = 0; j < c; j ++){	
+			scanf("%f", &matriz[i][j]);
+			media[i] += matriz[i][j];
+		}
+		media[i] = media[i]/c;
+	}
+
+	for (i = 0; i < l; i++){
+		for (j = 0; j < c; j++){
+			if (matriz[i][j] > media[i]){
+				cont++;
+			}
+		}
+		printf("A média da %d° linha é %f e %d números estão acima da média \n", i+1, media[i], cont);
+		cont = 0;
+	}
+	
+	return 0;
+}
